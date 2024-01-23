7a6e983dc825650240e95f68c4f1366fbe88bb88
CaglaOkmen <126767927+CaglaOkmen@users.noreply.github.com>
2024-01-24 01:31:59 +0300

    Create SepetEkrani.vue

---
+++ src/pages/SepetEkrani.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div>
+      <component v-for="(component, index) in components" :key="index" :is="component"></component>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import ThePage5Ö from 'src/components/ThePage5-ö.vue';
+
+export default defineComponent({
+  name: 'YeniUyelik', // Added comma here
+  components: {
+    ThePage5Ö
+  },
+  data() {
+    return {
+      components: ['ThePage5Ö']
+    }
+  }
+})
+</script>
+
+
+
+
