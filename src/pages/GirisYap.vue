499e8680d96b0dc02963542908520c647f4f53b4
CaglaOkmen <126767927+CaglaOkmen@users.noreply.github.com>
2024-01-24 01:31:19 +0300

    Create GirisYap.vue

---
+++ src/pages/GirisYap.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div>
+      <component v-for="(component, index) in components" :key="index" :is="component"></component>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import ThePage5 from 'src/components/ThePage5.vue';
+
+export default defineComponent({
+  name: 'YeniUyelik', // Added comma here
+  components: {
+    ThePage5
+  },
+  data() {
+    return {
+      components: ['ThePage5']
+    }
+  }
+})
+</script>
+
+
+
+
